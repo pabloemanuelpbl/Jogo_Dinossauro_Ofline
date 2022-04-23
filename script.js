77b25d48d046eab985a8c4e04209c06855613475
Pablo <pablo.fju.pa@gmail.com>
2022-04-23 01:55:30 -0300

    versao_estavel

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+const aperteTab = document.querySelector('.aperteTab')
 
+function start(image){
+    dino.style.background = `url(imagens/${image}.png)`;
+}
 
 function eventaction(event){
+    if(!jump.start){
+        jump.start = true;
+        createCactus();
+        aperteTab.innerText = "";
+    }
+
+    if(jump.gameOver){
+        document.location.reload();
+        jump.start = false;
+    }
     if(event.keyCode === 32){
         console.log('pressionou')
         if(!jump.isJump){
             jump()
         }
     }
+    
 }
 
 //pulo
@@ -25,25 +40,37 @@ function jump(){
                     clearInterval(dowInterval)
                     jump.isJump = false
                 } else {
-                    jump.position -= 20;
+                    jump.position -= 25;
                     dino.style.bottom = jump.position + 'px';
                 }
             }, 30)
         } else {
-            jump.position += 20;
+            jump.position += 25;
             dino.style.bottom = jump.position + 'px'; 
         } 
-    }, 30)
+    }, 20)
 }
 
 function createCactus(){
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
     let randomTime = Math.random() * 6000;
+    let randomImage = Math.random() * 10; 
+
+    function selectImage(valor){
+        if(valor > 9){
+            cactus.style.background = `url(imagens/${selectImage.arr[1]}.png)`;
+        } else
+        if(valor > 7){
+            cactus.style.background = `url(imagens/${selectImage.arr[2]}.png)`;
+        }
+    }
+    selectImage.arr = ["cactus", "marge_simpson", "bart_pelado"]
 
     cactus.classList.add('cactus');
     cactus.style.left = cactusPosition + 'px';
     background.appendChild(cactus);
+    selectImage(randomImage);
 
     //movimento do cactu
     let leftInterval = setInterval(()=>{
@@ -52,7 +79,7 @@ function createCactus(){
             background.removeChild(cactus);
         } else if(cactusPosition > 0 && cactusPosition < 60 && (jump.position < 60 || jump.position == undefined) ){
             //game over
-            console.log(jump.position)
+            jump.gameOver = true;
             clearInterval(leftInterval);
             document.body.innerHTML = '<h1 class="game_over">fim de jogo</h1>'
         } else {
@@ -62,9 +89,8 @@ function createCactus(){
     }, 20)
 
     //recursividade ( massa! )
-    setTimeout(createCactus, randomTime);
+    if (!jump.gameOver) setTimeout(createCactus, randomTime);
 }
 
-createCactus()
 //eventos
 document.addEventListener('keyup', eventaction);
